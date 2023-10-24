ffeac368820958e6f4bf79ad94a1914cd338ab70
safaanilatasoy <safaanilatasoy@gmail.com>
2023-10-24 12:46:54 +0300

    Instaling Nodemon

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const fn = function () {
-  console.log('Function Working');
-};
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req,res) => {
+    const photo = {
+        id: 1,
+        name: "Photo Name",
+        description: "Photo Desc"
+    }
+    res.send(photo)
+})
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
