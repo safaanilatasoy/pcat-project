95b817628b7fd17582e78fad83a315fbe0e5142f
safaanilatasoy <safaanilatasoy@gmail.com>
2023-11-10 17:40:51 +0300

    add photos and connect database parts

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.set('view engine', 'ejs');
 
 // MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+
 
 // ROUTES
 app.get('/', (req, res) => {
@@ -20,6 +23,12 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
   res.render('add');
 });
+app.post('/photos', (req, res) => {
+  console.log(req.body);
+  res.redirect('/')
+});
+
+
 
 const port = 3000;
 
