0515488d9a65a562703ba6e203f96ca8d810059a
safaanilatasoy <safaanilatasoy@gmail.com>
2023-11-12 13:45:16 +0300

    Veritabanı işlemleri - (mongoose)

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
 
 const app = express();
+
+// Connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db');
 
 // TEMPLATE ENGINE
 app.set('view engine', 'ejs');
@@ -14,8 +19,11 @@ app.use(express.json());
 
 
 // ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({})
+  res.render('index',{
+    photos
+  });
 });
 app.get('/about', (req, res) => {
   res.render('about');
@@ -23,8 +31,9 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
   res.render('add');
 });
-app.post('/photos', (req, res) => {
-  console.log(req.body);
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body)
   res.redirect('/')
 });
 
